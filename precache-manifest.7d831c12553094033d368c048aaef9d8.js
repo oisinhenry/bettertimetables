e8cbcd6ef1c016388a98be4eeb92bf4b0eccf4b5
Oisin <oisinhenry@gmail.com>
2020-03-06 04:21:15 +0000

    Updates

---
+++ precache-manifest.7d831c12553094033d368c048aaef9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917f0285e3616037833b67ec96a792fc",
+    "url": "/bettertimetables/index.html"
+  },
+  {
+    "revision": "20c5d087961a8b6bd183",
+    "url": "/bettertimetables/static/css/main.4ef6c6ef.chunk.css"
+  },
+  {
+    "revision": "7166c1693a10d37ef3e5",
+    "url": "/bettertimetables/static/js/2.b2aec45c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bettertimetables/static/js/2.b2aec45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c5d087961a8b6bd183",
+    "url": "/bettertimetables/static/js/main.42a13d19.chunk.js"
+  },
+  {
+    "revision": "5b0f543b7bc4d6891b52",
+    "url": "/bettertimetables/static/js/runtime-main.9fa44695.js"
+  }
+]);
