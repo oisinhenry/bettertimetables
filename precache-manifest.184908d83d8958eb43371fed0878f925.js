8e501cf8272d7136694ecc1ec5f764d9eb267cea
Oisin <oisinhenry@gmail.com>
2020-03-06 04:00:19 +0000

    initial commit

---
+++ precache-manifest.184908d83d8958eb43371fed0878f925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5320650c0593e6e0f9dfe1a5d85897a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4988554e85efcd4819de",
+    "url": "/static/css/main.4ef6c6ef.chunk.css"
+  },
+  {
+    "revision": "7166c1693a10d37ef3e5",
+    "url": "/static/js/2.b2aec45c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b2aec45c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4988554e85efcd4819de",
+    "url": "/static/js/main.c945501e.chunk.js"
+  },
+  {
+    "revision": "6816fc0b528c902094f4",
+    "url": "/static/js/runtime-main.08e1921f.js"
+  }
+]);
